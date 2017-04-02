981edca067ac5f138973ecf48700af05f4cd66e4
opuerto <oscar.puertohn@gmail.com>
2017-04-02 02:54:31 +0000

    Add Get info from sync

--- Musebots/op_piano/piano/src/main/java/tools/ensemble/behaviours/ComposerBhaviours/accompaniment/RequestInfoSectionToSync.java
+++ Musebots/op_piano/piano/src/main/java/tools/ensemble/behaviours/ComposerBhaviours/accompaniment/RequestInfoSectionToSync.java
@@ -1,27 +1,57 @@
 package tools.ensemble.behaviours.ComposerBhaviours.accompaniment;
 
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+import tools.ensemble.agents.Composer;
+import tools.ensemble.interfaces.DataStoreComposer;
 
 /**
- * Created by OscarAlfonso on 3/28/2017.
+ * Created by OscarAlfonso on 3/29/2017.
  */
-public class RequestInfoSectionToSync extends OneShotBehaviour {
+public class RequestInfoSectionToSync extends OneShotBehaviour implements DataStoreComposer {
 
-    private int transition;
-
+    private int transition = 13;
+    private int firstTimeHere = 0;
+    private AID SynId;
     public RequestInfoSectionToSync(Agent a)
     {
         super(a);
     }
 
-    public void action()
-    {
+    public void action() {
+        if (Composer.firstTime < 1){
 
+            ACLMessage requestInfo = new ACLMessage(ACLMessage.REQUEST);
+            requestInfo.setConversationId("request-Current-Section-to-Syn");
+            requestInfo.setReplyWith(myAgent.getLocalName() + System.currentTimeMillis());
+            int firstimeAccompaniement = 1;
+            requestInfo.setContent(String.valueOf(firstimeAccompaniement));
+            if (getDataStore().containsKey(COMPOSER_MY_INTERNAL_SYNCHRONIZER)) {
+                SynId = (AID) getDataStore().get(COMPOSER_MY_INTERNAL_SYNCHRONIZER);
+                System.out.println(SynId);
+            }
+
+            requestInfo.addReceiver(SynId);
+            getDataStore().put(CURRENT_MESSAGE_FOR_SYN, requestInfo);
+            myAgent.send(requestInfo);
+            transition = 13;
+            System.out.println("receiver "+SynId);
+
+
+        }
     }
 
     public int onEnd()
     {
+        //firstTimeHere++;
+        Composer.firstTime++;
+        if(transition == 2)
+        {
+            block(1000);
+        }
         return transition;
+
     }
 }
